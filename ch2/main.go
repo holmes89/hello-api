d64ff300d7a513b6a2bf205a183596974dac89aa
Joel Holmes <holmes89@gmail.com>
2021-04-22 15:48:25 -0400

    ch2 code

---
+++ ch2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	addr := ":8080"
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		enc := json.NewEncoder(w)
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		resp := map[string]string{"hello": "world"}
+		if err := enc.Encode(resp); err != nil {
+			panic("unable to encode response")
+		}
+	})
+
+	fmt.Printf("listening on %s\n", addr)
+
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
