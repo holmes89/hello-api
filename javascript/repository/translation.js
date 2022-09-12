ebc1abcf4bbdfc4af57f3b448b8cdf9b66a500b5
Joel Holmes <holmes89@gmail.com>
2022-09-12 14:54:42 -0400

    fixes

--- javascript/repository/translation.js
+++ javascript/repository/translation.js
@@ -1,17 +1,17 @@
-const redis = require("redis");
+const redis = require('redis');
 
 class Repository {
   constructor(host, port) {
-    this.host = host ? host : process.env.DB_HOST || "localhost";
-    this.port = port ? port : process.env.DB_PORT || "6379";
-    this.defaultLanguage = process.env.DEFAULT_LANGUAGE || "english";
+    this.host = host ? host : process.env.DB_HOST || 'localhost';
+    this.port = port ? port : process.env.DB_PORT || '6379';
+    this.defaultLanguage = process.env.DEFAULT_LANGUAGE || 'english';
     const connectionURL = `redis://${this.host}:${this.port}`;
     console.log(`connecting to ${connectionURL}`);
     this.client = redis.createClient({ url: connectionURL });
-    this.client.on("connect", () => {
-      console.log("connected to redis");
+    this.client.on('connect', () => {
+      console.log('connected to redis');
     });
-    this.client.on("error", (err) => console.log("client error", err));
+    this.client.on('error', (err) => console.log('client error', err));
     this.client.connect();
   }
 
--- javascript/routes/translation.js
+++ javascript/routes/translation.js
@@ -1,10 +1,10 @@
 const translation = (translationService) => {
   return async (req, res) => {
-    let language = req.query.language || "english";
+    let language = req.query.language || 'english';
     const resp = await translationService.translate(language, req.params.word);
     resp
       ? res.json({ language: language.toLowerCase(), translation: resp })
-      : res.status(404).send("Missing translation");
+      : res.status(404).send('Missing translation');
   };
 };
 
