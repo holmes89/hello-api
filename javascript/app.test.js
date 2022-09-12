4854d924b92b6b910b3619c1ee985763b27b90a0
Joel Holmes <holmes89@gmail.com>
2022-09-12 14:56:56 -0400

    resolve things

--- javascript/app.test.js
+++ javascript/app.test.js
@@ -7,35 +7,35 @@ let app;
 let api;
 
 beforeAll(async () => {
-  container = await new GenericContainer('redis')
-    .withExposedPorts(6379)
-    .withCopyFileToContainer('./data/dump.rdb', '/data/dump.rdb')
-    .start();
-  const port = container.getMappedPort(6379);
-  const host = container.getHost();
-  api = new App(host, port);
-  app = api.app;
+	container = await new GenericContainer('redis')
+		.withExposedPorts(6379)
+		.withCopyFileToContainer('./data/dump.rdb', '/data/dump.rdb')
+		.start();
+	const port = container.getMappedPort(6379);
+	const host = container.getHost();
+	api = new App(host, port);
+	app = api.app;
 });
 
 afterAll(async () => {
-  await api.close();
-  await container.stop();
+	await api.close();
+	await container.stop();
 });
 
 describe('Translate', () => {
-  test('hello translation in english to be hello', async () => {
-    const response = await request(app).get('/translate/hello');
-    expect(response.body).toEqual({
-      translation: 'Hello',
-      language: 'english',
-    });
-    expect(response.statusCode).toBe(200);
-    return response;
-  });
-  test('hello translation in german to be hallo', async () => {
-    const response = await request(app).get('/translate/hello?language=GERMAN');
-    expect(response.body).toEqual({ translation: 'Hallo', language: 'german' });
-    expect(response.statusCode).toBe(200);
-    return response;
-  });
+	test('hello translation in english to be hello', async () => {
+		const response = await request(app).get('/translate/hello');
+		expect(response.body).toEqual({
+			translation: 'Hello',
+			language: 'english',
+		});
+		expect(response.statusCode).toBe(200);
+		return response;
+	});
+	test('hello translation in german to be hallo', async () => {
+		const response = await request(app).get('/translate/hello?language=GERMAN');
+		expect(response.body).toEqual({ translation: 'Hallo', language: 'german' });
+		expect(response.statusCode).toBe(200);
+		return response;
+	});
 });
--- javascript/routes/translation.js
+++ javascript/routes/translation.js
@@ -1,11 +1,11 @@
 const translation = (translationService) => {
-  return async (req, res) => {
-    let language = req.query.language || 'english';
-    const resp = await translationService.translate(language, req.params.word);
-    resp
-      ? res.json({ language: language.toLowerCase(), translation: resp })
-      : res.status(404).send('Missing translation');
-  };
+	return async (req, res) => {
+		let language = req.query.language || 'english';
+		const resp = await translationService.translate(language, req.params.word);
+		resp
+			? res.json({ language: language.toLowerCase(), translation: resp })
+			: res.status(404).send('Missing translation');
+	};
 };
 
 module.exports = translation;
