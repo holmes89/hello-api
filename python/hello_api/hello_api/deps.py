ef35a412e33cd78e74888fb4e68234af67540f34
Joel Holmes <holmes89@gmail.com>
2022-09-12 19:29:25 -0400

    Fix build

--- python/hello_api/hello_api/deps.py
+++
@@ -1,6 +0,0 @@
-from hello_api.repo import RepositoryInterface
-from hello_api.redis import RedisRepository
-
-
-def redis_client() -> RepositoryInterface:
-    return RedisRepository()
--- python/hello_api/hello_api/repo.py
+++
@@ -1,4 +0,0 @@
-class RepositoryInterface:
-    def translate(self, language: str, word: str) -> str:
-        """translates word into given language"""
-        pass
